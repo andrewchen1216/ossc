59e592a444b63784724d9d54078e3ecb23dffa8e
Makerspace <makerspace@schiller-offenburg.de>
2020-07-02 16:53:04 +0200

    Updated KeypadController
    - Looks much more clean now (yay)
    - has the potential to do stuff

--- keypadController/main.c
+++ keypadController/main.c
@@ -1,68 +1,44 @@
 /*
  * KeypadDecoder.c
- *
  * Created: 02.07.2020 10:36:52
  * Author : memecian
- 
  */ 
 
 #include <avr/io.h>
 #include <stdbool.h>
+#include <util/twi.h>
+int fuckit(int x, int y)
+{
+	// do some code that sends button code per i²c
+}
 
 
 int main(void)
 {	
-	bool row[9];
 
-	DDRA = 0b00011111; //0 stands for input, 1 stands for output
+	bool row[9];
+	bool col[5] = {PORTA0, PORTA1, PORTA2, PORTA3, PORTA4}; //columns left to right
+	TWSAM = 0x24;		//set I²C address mask
+	
+	DDRA = 0b00011111;	//0 stands for input, 1 stands for output
 	DDRB = 0b00001111;
 	DDRC = 0b00100100;	
 	
     while (1) 
     {
 		row[] = {PORTA7, PORTA6, PORTA5, PORTC5, PORTC2, PORTB3, PORTB2, PORTB1, PORTB0}; //rows top to bottom 
-		PORTA0 = 1;
-		for (int i = 0; i < 5; i++) 
+		for (int i = 0; i < 5; i++)
 		{
-			if (row[0])
+			col[i] = 1;
+			for (int j = 0; j < 9; j++)
 			{
-				
+				if (row[j] == 0)
+				{
+					fuckit(i,j);
+				}
 			}
-			if (row[1])
-			{
-				
-			}
-			if (row[2])
-			{
-				
-			}
-			if (row[3])
-			{
-				
-			}
-			if (row[4])
-			{
-				
-			}
-			if (row[5])
-			{
-				
-			}
-			if (row[6])
-			{
-				
-			}
-			if (row[7])
-			{
-				
-			}
-			if (row[8])
-			{
-				
-			}
-
+			col[i] = 0;
 		}
-    
 	}
 }
 
