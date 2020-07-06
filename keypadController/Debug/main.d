1563650af600a9129f4caef1373cf6e2ce1a5ced
memecian <55235293+memecian@users.noreply.github.com>
2020-07-06 16:40:15 +0200

    KeypadController rebuild

--- keypadController/Debug/main.d
+++ keypadController/Debug/main.d
@@ -11,7 +11,10 @@ main.d main.o: .././main.c \
  c:\absolut\ alkoholische\ anwendungen\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  c:\absolut\ alkoholische\ anwendungen\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
  c:\absolut\ alkoholische\ anwendungen\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h \
- c:\absolut\ alkoholische\ anwendungen\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\twi.h
+ c:\absolut\ alkoholische\ anwendungen\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\twi.h \
+ c:\absolut\ alkoholische\ anwendungen\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\absolut\ alkoholische\ anwendungen\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\absolut\ alkoholische\ anwendungen\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
 
 c:\absolut\ alkoholische\ anwendungen\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -38,3 +41,9 @@ c:\absolut\ alkoholische\ anwendungen\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-
 c:\absolut\ alkoholische\ anwendungen\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h:
 
 c:\absolut\ alkoholische\ anwendungen\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\twi.h:
+
+c:\absolut\ alkoholische\ anwendungen\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\absolut\ alkoholische\ anwendungen\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\absolut\ alkoholische\ anwendungen\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
